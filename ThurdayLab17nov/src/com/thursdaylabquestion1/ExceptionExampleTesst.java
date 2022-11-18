28d637e0736e8913044b5478811fc7f8c42e4abb
balu6234 <113251205+balu6234@users.noreply.github.com>
2022-11-18 11:33:12 +0530

    Update ExceptionExampleTesst.java

--- ThurdayLab17nov/src/com/thursdaylabquestion1/ExceptionExampleTesst.java
+++ ThurdayLab17nov/src/com/thursdaylabquestion1/ExceptionExampleTesst.java
@@ -1,28 +1,25 @@
 package com.thursdaylabquestion1;
 import java.util.*;
+
+
 public class ExceptionExampleTesst {
   void squarecheck(int a)throws OutofRangeException{
 	  Scanner sc=new Scanner(System.in);
-	  a=a*a;
-	  if(a<10 && a>50){
-		  throw new OutofRangeException("Out of Range");
-	  }
+	  System.out.println("enter  number :");
+	a=sc.nextInt();
+	  if (a>10 && a<50)
+		  throw new OutofRangeException("invalid range");
 	  else
-	  {
-		  throw(new OutofRangeException("Square is"+a));
+		  throw new OutofRangeException("Square is"+a*a);
+  }
+  
+  public static void main(String[] args) {
+	  ExceptionExampleTesst ext=new ExceptionExampleTesst();
+	  try {
+		  ext.squarecheck(12);
+	  }
+	  catch(Exception e) {
+		  System.out.println(e);
 	  }
   }
-public static void main(String[] args) {
-	{
-		ExceptionExampleTesst ext=new ExceptionExampleTesst();
-		try
-		{
-			ext.squarecheck(1);
-		}
-		catch(OutofRangeException e)
-		{
-			System.out.println(e);
-		}
-		}
-	}
 }
